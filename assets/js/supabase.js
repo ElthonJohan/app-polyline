44a69cac98d20eb69d383e3f40e31388995d1362
ElthonJohan <jg171020202004@gmail.com>
2026-06-21 21:50:24 -0500

    configuracion para migracion a supabase

---
+++ assets/js/supabase.js
@@ -0,0 +1,12 @@
+const SUPABASE_URL =
+  "https://lxobuhdibnbpkknxlezz.supabase.co";
+
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4b2J1aGRpYm5icGtrbnhsZXp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIwODQzNzIsImV4cCI6MjA5NzY2MDM3Mn0.ixaEPR3tM4kJfze7fBFsEnHtmV3O8CvoVHNxgCQiWIY";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
+
+console.log("Supabase conectado");
